ba33b997a80199fdb37785e1cbc27ceef53fcbf5
Nikhil4498 <nikhilcherian3@gmail.com>
2024-09-29 17:33:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,5 +76,6 @@ def addProduct():
 
   return jsonify('Product was successfully added.')
 
-if __name__=='__main__':
-  app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
